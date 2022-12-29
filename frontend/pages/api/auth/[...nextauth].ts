8817938dcf7bafbf943c193e00405f725d7dd695
Filipe Aleixo <filipe.b.aleixo@gmail.com>
2022-12-29 12:26:12 +0000

    Add e-mail sign in

--- frontend/pages/api/auth/[...nextauth].ts
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google"
+import EmailProvider from "next-auth/providers/email";
 import { JWT } from "next-auth/jwt";
 import { HasuraAdapter } from "next-auth-hasura-adapter";
 import * as jsonwebtoken from "jsonwebtoken";
@@ -12,6 +13,10 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM
     }),
   ],
   adapter: HasuraAdapter({
