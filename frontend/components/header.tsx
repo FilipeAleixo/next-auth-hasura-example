72d7d9201df2886c110cdae456d234a1a8d1b7ae
Filipe Aleixo <filipe.b.aleixo@gmail.com>
2023-01-02 15:39:10 +0000

    Fix avatar

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -41,7 +41,12 @@ export default function Header() {
             <>
               {session.user.image && (
                 <span
-                  style={{ backgroundImage: `url('${session.user.image}')` }}
+                  style={{ 
+                    backgroundImage: `url('${session.user.image}')`,
+                    backgroundPosition: 'center',
+                    backgroundSize: 'cover',
+                    backgroundRepeat: 'no-repeat' 
+                  }}
                   className={styles.avatar}
                 />
               )}
